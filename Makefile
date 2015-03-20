9cf172690abc2fedfb2fd3e9af1f5b42df2d00a6
samuel.cheng@advantech.com.tw <samuel.cheng@advantech.com.tw@9d855a72-824a-9447-89d5-058a706077a7>
2015-03-20 09:13:15 +0000

    add timeout test
    git-svn-id: https://172.20.2.44/svn/iagicom/driver/icom_pseudo_tty_driver/testtool/system@3985 9d855a72-824a-9447-89d5-058a706077a7

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: mctrl openclose poll qfsize stress reconn
+all: mctrl openclose poll qfsize stress reconn timeout
 
 mctrl: mctrl.c
 	gcc -Wall $^ -o $@
@@ -12,6 +12,8 @@ stress: stress.c
 	gcc -Wall $^ -o $@
 reconn: reconn.c
 	gcc -Wall $^ -o $@
+timeout: timeout.c
+	gcc -Wall $^ -o $@
 clean:
 	rm -f mctrl
 	rm -f openclose
@@ -19,3 +21,4 @@ clean:
 	rm -f qfsize
 	rm -f stress
 	rm -f reconn
+	rm -f timeout
---
+++ timeout.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+//#include <termios.h>
+#include <asm-generic/termbits.h>
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int ret;
+	int count;
+	int opt;
+	struct termios2 newtio;
+	unsigned int input, output;
+	count = 0;
+	
+	if(argc != 2){
+		printf("%s [tty]\n", argv[0]);
+		return 0;
+	}
+
+	fd = open(argv[1], O_RDWR);
+	if(fd < 0){
+		printf("failed to open fd\n");
+		return 0;
+	}
+
+
+	ret = ioctl(fd, TCGETS2, &newtio);
+	newtio.c_iflag &= ~(ISTRIP|IUCLC|IGNCR|ICRNL|INLCR|ICANON|IXON|PARMRK);
+	newtio.c_iflag |= (IGNBRK|IGNPAR);
+	newtio.c_lflag &= ~(ECHO|ICANON|ISIG);
+	newtio.c_cflag &= ~CBAUD;
+	newtio.c_cflag |= BOTHER;//|CRTSCTS;
+	newtio.c_cflag &= (~CRTSCTS);
+	newtio.c_ospeed = 921600;
+	newtio.c_ispeed = 921600;
+	ret = ioctl(fd, TCSETS2, &newtio);
+	ret = ioctl(fd, TCGETS2, &newtio);
+
+
+	count = 100;
+
+	do{
+		output = 0;
+		opt = (count%4);
+
+		switch(opt){
+		case 0:
+		default:
+			break;
+		case 1:
+			output |= TIOCM_DTR;
+			break;
+		case 2:
+			output |= TIOCM_RTS;
+			break;
+		case 3:
+			output |= (TIOCM_DTR|TIOCM_RTS);
+			break;
+		}
+	
+		ret = ioctl(fd, TIOCMSET, &output);
+		if(ret){
+			printf("failed to set\n");
+			return 0;
+		}
+		sleep(1);
+		ret = ioctl(fd, TIOCMGET, &input);
+		if(ret){
+			printf("failed to get\n");
+			return 0;
+		}
+
+		printf("\rDTR");
+		if(output & TIOCM_DTR){
+			printf("*");
+		}else{
+			printf("_");
+		}
+		
+		printf("RTS");
+		if(output & TIOCM_RTS){
+			printf("*");
+		}else{
+			printf("_");
+		}
+
+		printf("CTS");
+		if(input & TIOCM_CTS){
+			printf("*");
+		}else{
+			printf("_");
+		}
+		printf("DSR");
+		if(input & TIOCM_DSR){
+			printf("*");
+		}else{
+			printf("_");
+		}
+		
+		printf("CAR");
+		if(input & TIOCM_CAR){
+			printf("*");
+		}else{
+			printf("_");
+		}
+		printf("RI");
+		if(input & TIOCM_RI){
+			printf("*");
+		}else{
+			printf("_");
+		}
+		printf("...   ");
+
+		fflush(stdout);
+		sleep(9);
+
+	}while(--count > 0);
+
+
+
+	close(fd);
+
+	return 0;
+}
+
